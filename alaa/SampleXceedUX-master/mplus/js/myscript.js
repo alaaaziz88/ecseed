16f322d640a3b0f0771276ab0db62c3f44571197
Alaaaziz88 <alaaaziz88@gmail.com>
2015-08-12 16:37:30 +0300

    a
    aa

---
+++ alaa/SampleXceedUX-master/mplus/js/myscript.js
@@ -0,0 +1,13 @@
+
+function load() {
+	var myarray = JSON.parse(data);
+	myFunction(arr) {
+    var out = "";
+    var i;
+    for(i = 0; i < arr.length; i++) {
+        out += '<a href="' + arr[i].url + '">' + 
+        arr[i].display + '</a><br>';
+    }
+    document.getElementById("id01").innerHTML = out;
+
+}
